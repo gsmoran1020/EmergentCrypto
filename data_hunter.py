cb0bf0d64042e52b92806d1a55172bed4cc69f4b
Gavin <92048502+gsmoran1020@users.noreply.github.com>
2021-11-12 17:30:35 -0500

    Update data_hunter.py

--- data_hunter.py
+++ data_hunter.py
@@ -11,7 +11,7 @@ def __get_most_recent(num: int) -> list[str]:
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--headless')
-    options.binary_location = os.environ.get('GOOGLE_CHROME_PATH') 
+    options.binary_location = os.environ.get('GOOGLE_CHROME_BIN') 
     driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH')  ,options=options)
     driver.get('https://www.coingecko.com/en/coins/recently_added')
 
